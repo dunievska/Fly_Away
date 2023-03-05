1dfb0f526d75097f7214accac4f00feda7b44525
Emilia Duniewska <kamienie@gmail.com>
2023-03-05 11:15:39 +0100

    first

---
+++ dist/index.a1531e54db25e283fd4f.bundle.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+fetch("https://raw.githubusercontent.com/dunievska/Fly_Away/main/api/data.json").then(function (response) {
+  return response.json();
+}).then(function (data) {
+  console.log(data.destinations);
+});
+/******/ })()
+;
+//# sourceMappingURL=index.a1531e54db25e283fd4f.bundle.js.map
--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
-const sumPlik = require("./sum")
-import * as wsb from "./sum2"
-import style from './css/index.scss'
-console.log(wsb.sum2(1,2))
-console.log(wsb.zmienna2)
-console.log("Hello world");
-console.log("suma wynosi " + sumPlik.sum(2,4))
-console.log("zmienna wynosi " + sumPlik.zmienna)
-
-let heading = document.querySelector("#demo"),
-    sumValue = sumPlik.sum(4554645,10)
-
-heading.innerText = `0+11110 = ${sumValue}`;
+fetch("https://raw.githubusercontent.com/dunievska/Fly_Away/main/api/data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data.destinations);
+  });
